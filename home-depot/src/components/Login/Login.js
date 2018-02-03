84b3179be4d68dad1b2701e031cbba23a280c8e5
Aseel Al Dallal <aseelaldallal@gmail.com>
2018-02-03 14:13:36 -0500

    basic login page

--- home-depot/src/components/Login/Login.js
+++ home-depot/src/components/Login/Login.js
@@ -2,11 +2,23 @@
 import React from 'react';
 // Images
 import Apron from '../../assets/images/apron.png';
+// Components
+import Button from '../UI/Button/Button';
 // Styling
 import classes from './Login.css';
 
 const Login = () => {
-  return <img src={Apron} alt="apron" />;
+  return (
+    <div className={classes.loginPage}>
+      <div className={classes.imageBox}>
+        <img src={Apron} alt="apron" />
+      </div>
+      <div className={classes.spacer} />
+      <div className={classes.loginContainer}>
+        <h1> DEPOT CLASH </h1>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
