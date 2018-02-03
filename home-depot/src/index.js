7f8d5f3518ff534c2d98cbb7ec929e5df543efea
mmurra88 <mmurra88@uwo.ca>
2018-02-03 13:00:42 -0500

    webpage says hi

--- home-depot/src/index.js
+++ home-depot/src/index.js
@@ -1,3 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+const App = () => (
+
+	<div> hi </div>
+)
+
+	
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
